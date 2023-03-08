5c95bd0a4c81673152813f9fca104fba6b6a50a7
blister-pack <andrecrujo@hotmail.com>
2023-03-08 21:52:34 +0100

    started matrix exercise - trouble w columns

---
+++ 8_matrix/matrix.py
@@ -0,0 +1,16 @@
+class Matrix:
+    def __init__(self, matrix_string):
+        pass
+
+    def row(self, index):
+        #  this method returns a row with the selected index
+        pass
+
+    def column(self, index):
+        #  this method returns a column with the selected index
+        pass
+
+#-----------------------------------------------------------------------------------------
+
+matrix = Matrix("9 8 7\n5 3 2\n6 6 7\n")
+print("9 8 7\n5 3 2\n6 6 7")
