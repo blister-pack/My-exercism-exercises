7e335fe036da722336b0692bb271914103c2be63
blister-pack <119133995+blister-pack@users.noreply.github.com>
2023-04-15 16:06:04 +0200

    a

--- 10_garden/garden.py
+++ 10_garden/garden.py
@@ -1,3 +1,14 @@
 class Garden:
     def __init__(self, diagram, students):
+        lol = f'{diagram} potato'
         pass
+
+    def plants(self, student_name):
+        pass
+
+
+
+
+#--------------------------------------------------------------------
+
+garden1 = Garden("VRCC\nVCGG", students=["Valorie", "Raven"])
