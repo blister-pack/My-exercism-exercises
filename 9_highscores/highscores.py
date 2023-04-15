5b7b1f6b6c3d02cfd4076cce5b3ef13e1beb3f81
blister-pack <119133995+blister-pack@users.noreply.github.com>
2023-04-15 15:46:26 +0200

    highscores exercise finished

--- 9_highscores/highscores.py
+++ 9_highscores/highscores.py
@@ -1,28 +1,22 @@
 class HighScores:
     def __init__(self, scores):
-        self.scores_list = scores
+        self.scores = scores
 
-    def highscore(self):
-        return max(self.scores_list)
+    def personal_best(self):
+        return max(self.scores)
 
-    def last_score(self):
-        return self.scores_list[-1]
+    def latest(self):
+        return self.scores[-1]
 
-    def podium(self):
-        high_scores = []
-        for score in self.scores_list:
-            high_scores.append(score)
-            high_scores.sort()
-            if len(high_scores) > 3:
-                high_scores.pop(0)
-        return high_scores
-
+    def personal_top_three(self):
+        high_scores = sorted(self.scores, reverse=True)
+        return high_scores[0:3]
 
 
 # --------------------------------------------------------
 
 scores1 = HighScores([30, 50, 20, 70])
-print(scores1.scores_list)
-print(scores1.highscore())
-print(scores1.podium())
-print(scores1.last_score())
+print(scores1.personal_top_three())
+
+scores2 = HighScores([10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70])
+print(scores2.personal_top_three())
